a737815e3d825adc2248d2538ecd4b3aac490910
Manuel Raimann <raimannma@outlook.de>
2024-09-24 22:09:00 +0200

    Add Monitoring

--- deadlock_api/main.py
+++ deadlock_api/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from starlette.responses import RedirectResponse
 
 from deadlock_api.routers import base, v1
@@ -11,6 +12,8 @@ app = FastAPI(
     title="Deadlock Data API",
     description="API for Deadlock game data, containing builds and active matches",
 )
+
+Instrumentator().instrument(app).expose(app, include_in_schema=False)
 
 app.include_router(base.router, include_in_schema=False)
 app.include_router(v1.router)
