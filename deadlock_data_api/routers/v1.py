25f7084f9f762b57530fad52837d6ac18044d3ed
Manuel Raimann <raimannma@outlook.de>
2024-10-18 20:34:22 +0200

    Add Builds by Author

--- deadlock_data_api/routers/v1.py
+++ deadlock_data_api/routers/v1.py
@@ -36,6 +36,12 @@ def get_builds_by_hero_id(response: Response, hero_id: int) -> list[Build]:
     return load_builds_by_hero(hero_id)
 
 
+@router.get("/builds/by-hero-id/{hero_id}", response_model_exclude_none=True)
+def get_builds_by_author_id(response: Response, author_id: int) -> list[Build]:
+    response.headers["Cache-Control"] = f"public, max-age={CACHE_AGE_BUILDS}"
+    return load_builds_by_author(author_id)
+
+
 @ttl_cache(ttl=CACHE_AGE_BUILDS - 1)
 def load_builds() -> dict[int, list[Build]]:
     conn = sqlite3.connect("builds.db")
@@ -53,6 +59,16 @@ def load_builds_by_hero(hero_id: int) -> list[Build]:
     cursor = conn.cursor()
     query = "SELECT json(data) as builds FROM hero_builds WHERE hero = ?"
     cursor.execute(query, (hero_id,))
+    results = cursor.fetchall()
+    return [Build.model_validate_json(result[0]) for result in results]
+
+
+@ttl_cache(ttl=CACHE_AGE_BUILDS - 1)
+def load_builds_by_author(author_id: int) -> list[Build]:
+    conn = sqlite3.connect("builds.db")
+    cursor = conn.cursor()
+    query = "SELECT json(data) as builds FROM hero_builds WHERE author_id = ?"
+    cursor.execute(query, (author_id,))
     results = cursor.fetchall()
     return [Build.model_validate_json(result[0]) for result in results]
 
