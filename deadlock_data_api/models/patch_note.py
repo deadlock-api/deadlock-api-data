cad279696d7b1a96ebcbe7eac41af0e56ff18226
Manuel Raimann <raimannma@outlook.de>
2024-10-31 17:00:23 +0100

    Parse Patch Note pub date

--- deadlock_data_api/models/patch_note.py
+++ deadlock_data_api/models/patch_note.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel, ConfigDict, Field
+from email.utils import parsedate_to_datetime
+
+from black import datetime
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 
 class PatchNoteGuid(BaseModel):
@@ -19,7 +22,7 @@ class PatchNote(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
     title: str
-    pub_date: str = Field(..., validation_alias="pubDate")
+    pub_date: str | datetime = Field(..., validation_alias="pubDate")
     link: str
     guid: PatchNoteGuid
     author: str
@@ -27,3 +30,10 @@ class PatchNote(BaseModel):
     dc_creator: str = Field(..., validation_alias="dc:creator")
     content_encoded: str = Field(..., validation_alias="content:encoded")
     slash_comments: str = Field(..., validation_alias="slash:comments")
+
+    @field_validator("pub_date", mode="before")
+    @classmethod
+    def validate_pub_date(cls, value):
+        if isinstance(value, str):
+            return parsedate_to_datetime(value)
+        return value
